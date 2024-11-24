3acfaf2a70e1b7aa530c80bc3b2c354872e4c7d5
Kleber Vales <ksvales@outlook.pt>
2024-11-24 09:01:00 -0300

    Resolucao

--- src/principal/Resolucao.java
+++ src/principal/Resolucao.java
@@ -1,5 +1,19 @@
 package principal;
 
-public class Resolucao {
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
 
+
+class Singleton{
+    private Singleton() {}
+    
+    public String str;
+    
+    public static Singleton getSingleInstance() {
+        return new Singleton();
+    }
 }
